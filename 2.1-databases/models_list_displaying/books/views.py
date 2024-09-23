4ca83efa6d4f33e3383264899347ad20897ffa08
Nicki tst <kanieleoutiz@gmail.com>
2024-09-23 21:54:34 +0300

    Recovering all files after a resource push

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
+import datetime
+
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+
+from .models import Book
 
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+
+    books = Book.objects.all().order_by('author')
+
+    context = {'books': books}
+    return render(request, template, context)
+
+def book_by_date_view(request, pub_date):
+    template = 'books/book_by_date.html'
+
+    try:
+        selected_date = datetime.datetime.strptime(pub_date, '%Y-%m-%d').date()
+    except ValueError:
+        raise Http404('Некорректный формат даты')
+
+    book = get_object_or_404(Book, pub_date=selected_date)
+
+    previous_book = Book.objects.filter(pub_date__lt=selected_date).order_by('-pub_date').first()
+    next_book = Book.objects.filter(pub_date__gt=selected_date).order_by('pub_date').first()
+
+    context = {
+        'book': book,
+        'previous_book': previous_book,
+        'next_book': next_book,
+    }
+
     return render(request, template, context)
--- 2.1-databases/models_list_displaying/main/urls.py
+++ 2.1-databases/models_list_displaying/main/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from books.views import books_view
+from books.views import books_view, book_by_date_view
 
 urlpatterns = [
     path('', books_view, name='books'),
+    path('books/', books_view, name='books_view'),
+    path('books/<str:pub_date>/', book_by_date_view, name='book_by_date'),
     path('admin/', admin.site.urls),
 ]
