890e0f5b366f7737689da7acc0fd4c7c78238866
Nicki tst <kanieleoutiz@gmail.com>
2024-09-23 21:54:34 +0300

    Recovering all files after a resource push

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,13 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.URLField(max_length=200)
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(max_length=100, unique=True)
+
+    def __str__(self):
+        return f'{self.name}: {self.price}'
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+
+from phones.models import Phone
 
 
 def index(request):
@@ -7,11 +9,26 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+
+    sort_by = request.GET.get('sort', '')
+
+    if sort_by == 'name':
+        phones = Phone.objects.all().order_by('name')
+    elif sort_by == 'min_price':
+        phones = Phone.objects.all().order_by('price')
+    elif sort_by == 'max_price':
+        phones = Phone.objects.all().order_by('-price')
+    else:
+        phones = Phone.objects.all()
+
+    context = {'phones': phones, 'sort_by': sort_by}
+
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = get_object_or_404(Phone, slug=slug)
+    context = {'phone': phone}
+
     return render(request, template, context)
